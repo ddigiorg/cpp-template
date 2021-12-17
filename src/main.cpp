3d26f27576c5607fdd7f93b26586b91f71a24f3f
ddigiorg <ddigiorg@gmail.com>
2021-12-17 14:44:32 -0500

    added more functionality to main.cpp for reference

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,70 @@
 #include "source_file.hpp"
 
-#include <chrono>
-#include <iostream>
+#include <vector> // for vectors
+#include <chrono> // for high resolution clock
+#include <random> // for mersenne twister pseudo random number generator
+#include <cstdint> // for uint32_t
+#include <iostream> // for printing messages
 
 using namespace MyNamespace;
 
 int main(int argc, char *argv[]) {
 
+    // Basic class stuff
     MyClass mc;
-
-    auto t0 = std::chrono::high_resolution_clock::now();
     mc.myFunc();
-    auto t1 = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> diff = t1 - t0;
-    std::cout << "t=" << diff.count() << std::endl;
 
-    return 0;
+    // Example for shortening really long types
+    typedef std::chrono::high_resolution_clock hrc;
+
+    // High resolution timer setup
+    hrc::time_point t0, t1;
+    hrc::duration diff;
+
+    // Mersenne Twister setup
+    uint32_t seed = (uint32_t)hrc::now().time_since_epoch().count();
+    std::mt19937 rng(seed);
+
+    // Variables for multiply, xnor, and xor performance tests
+    const uint32_t NUM = 100000000;
+    std::vector<uint32_t> a(NUM);
+    std::vector<uint32_t> b(NUM);
+    std::vector<uint32_t> c(NUM);
+
+    // Randomize vectors
+    t0 = hrc::now();
+    for (uint32_t i=0; i<NUM; i++) {
+        a[i] = rng();
+        b[i] = rng();
+    }
+    t1 = hrc::now();
+    diff = t1 - t0;
+    std::cout << "ran=" << diff.count() / 1e9 << "s" << std::endl;
+
+    // Multiply test
+    t0 = hrc::now();
+    for (uint32_t i=0; i<NUM; i++) {
+        c[i] = a[i] * b[i];
+    }
+    t1 = hrc::now();
+    diff = t1 - t0;
+    std::cout << "mul=" << diff.count() / 1e9 << "s" << std::endl;
+
+    // XNOR test
+    t0 = hrc::now();
+    for (uint32_t i=0; i<NUM; i++) {
+        c[i] = ~(a[i] ^ b[i]);
+    }
+    t1 = hrc::now();
+    diff = t1 - t0;
+    std::cout << "xnr=" << diff.count() / 1e9 << "s" << std::endl;
+
+    // XOR Test
+    t0 = hrc::now();
+    for (uint32_t i=0; i<NUM; i++) {
+        c[i] = a[i] ^ b[i];
+    }
+    t1 = hrc::now();
+    diff = t1 - t0;
+    std::cout << "xor=" << diff.count() / 1e9 << "s" << std::endl;
 }
